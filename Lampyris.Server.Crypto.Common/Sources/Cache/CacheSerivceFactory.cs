3b09e698a59fb9ab771dbd981ded937b6b9d6845
wushuhong <390031942@qq.com>
2025-02-25 15:03:07 +0800

    2025/2/25

---
+++ Lampyris.Server.Crypto.Common/Sources/Cache/CacheSerivceFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Lampyris.Server.Crypto.Common;
+
+using System;
+using System.Collections.Concurrent;
+
+public enum CacheServiceType
+{
+    MemoryCache,
+    RedisCache
+}
+
+public static class CacheServiceFactory
+{
+    private static readonly ConcurrentDictionary<CacheServiceType, ICacheService> m_CacheServices = new();
+
+    public static ICacheService CreateCacheService(CacheServiceType cacheServiceType)
+    {
+        // 如果实例已存在，则直接返回；否则创建新实例并存储
+        return m_CacheServices.GetOrAdd(cacheServiceType, type =>
+        {
+            return type switch
+            {
+                CacheServiceType.MemoryCache => new MemoryCacheService(),
+                CacheServiceType.RedisCache => new RedisCacheService(),
+                _ => throw new ArgumentException($"Unsupported cache service type: {cacheServiceType}")
+            };
+        });
+    }
+}
